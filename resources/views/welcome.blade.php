0c86d224b06baf491e05f543fe2db040cbb96b4a
Unknown <primo39@gmail.com>
2018-02-17 00:32:30 -0300

    added Martin Ghiglia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel por Martin Ghiglia
                 </div>
 
                 <div class="links">
