e05dab4398c2f9d103664f65e4e7fc5a2d96764e
Fabian <fsellanes@gmail.com>
2018-02-20 01:01:37 -0300

    Creación del index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>FEOLA CASELLA & GONZALEZ FEOLA</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+  <link rel="stylesheet" href="{{ asset('css/general.css') }}"> 
+  
+  
+  <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" style="color:white; font-family: 'Slabo', serif;" href="#">FEOLA CASELLA & GONZALEZ FEOLA</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#nuestrafirma">NUESTRA FIRMA</a></li>
+        <li><a href="#misionvision">MISIÓN</a></li>
+        <li><a href="#misionvision">VISIÓN</a></li>
+        		
+		<li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">SERVICIOS
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#serviciosjuridicos">JURIDICOS</a></li>
+          <li><a href="#asesoramientointegrado">JURIDICO - CONTABLE</a></li>
+          </ul>
+      </li>
+		
+		 <li><a href="#prof">PROFESIONALES</a></li>
+		<li><a href="#derechos">SUS DERECHOS</a></li>
+		<li><a href="#contacto">CONTACTO</a></li>
+		
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<div class="jumbotron text-center">
+	<h2>Bienvenido a nuestro estudio</h2>
+	<h1 style="color:white; font-family: 'Slabo', serif;">FEOLA CASELLA & GONZALEZ FEOLA</h1> 
+	<h2>En defensa de sus derechos desde 1990</h2>
+	<h3>La Paloma - Rocha - Uruguay</h3>		
+</div>
+
+<div id="nuestrafirma" class="container-fluid contpadding">
+	<div class="row">			
+		<div class="col-xs-12 col-sm-4 col-sm-offset-2">
+			<h1 class="titulo">Nuestra Firma</h1>
+			<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+		</div>	
+		<div class="col-xs-12 col-sm-4">
+			<img src="img/reloj.jpg" class="img-responsive pull-right" alt="ESTUDIO FEOLA CASELLA & GONZALEZ FEOLA"/>
+		</div>					
+	</div>  
+</div>
+
+<div id="misionvision" class="container-fluid contpadding imgfondo darken">
+	<div class="row">			
+		<div class="col-xs-12 col-sm-4 col-sm-offset-2">
+			<h1 class="titulo slideanim">Misión</h1>
+			<p style="color:white;" class="slideanim">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</strong></p>
+		</div>	
+		<div class="col-xs-12 col-sm-4">
+			<h1 class="titulo slideanim">Visión</h1>
+			<p style="color:white;" class="slideanim">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+		</div>					
+	</div>  
+</div>
+
+<div id="serviciosjuridicos" class="container-fluid text-center contpadding">
+	<h1 class="titulo slideanim">Servicios jurídicos</h1>
+	<br>
+	<div class="row">
+		<div class="col-xs-12 col-sm-3">
+			<i class="fas fa-users fa-5x slideanim"></i>
+			<h3 class="slideanim">CIVIL</h3>
+			<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+		</div>
+		
+		<div class="col-xs-12 col-sm-3">
+			<i class="fas fa-gavel fa-5x slideanim"></i>
+			<h3 class="slideanim">PENAL</h3>
+			<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+		</div>
+	
+		<div class="col-xs-12 col-sm-3">
+			<i class="fas fa-home fa-5x slideanim"></i>
+			<h3 class="slideanim">FAMILIA</h3>
+			<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+		</div>
+		
+		<div class="col-xs-12 col-sm-3">
+			<i class="fas fa-university fa-5x slideanim"></i>
+			<h3 class="slideanim">LABORAL</h3>
+			<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+		</div>
+	</div>	
+</div>
+
+<div id="asesoramientointegrado" class="container-fluid cont-gris text-center contpadding">
+	<div class="row">		
+		<div class="col-xs-12 col-md-6 col-md-offset-3">
+			<h1 id="asesoramiento" class="slideanim">Asesoramiento empresarial integrado</h1>
+			<h3 class="slideanim">JURÍDICO - CONTABLE</h3>
+			<p class="slideanim">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+		</div>
+	</div>  
+</div>
+
+<div id="prof" class="container-fluid text-center contpadding">
+	<h1 id="profesionales">Profesionales</h1>
+	<br>
+	<div class="row text-center">
+		<div class="col-xs-12 col-sm-4 col-sm-offset-2">			
+				<img src="img/abogado.png" class="img-responsive center-block slideanim" alt="Dr. Cecilia González Feola"/>
+				<h3 class="slideanim"><strong>Dra. Cecilia González Feola</strong></h3>
+				<p>- blablabla</p>
+				<p>- blablabla</p>
+				<p>- blablabla</p>			
+		</div>
+		<div class="col-xs-12 col-sm-4">			
+				<img src="img/abogado.png" class="img-responsive center-block slideanim" alt="Dr. Cecilia González Feola"/>
+				<h3 class="slideanim"><strong>Dra. Cecilia González Feola</strong></h3>
+				<p>- blablabla</p>
+				<p>- blablabla</p>
+				<p>- blablabla</p>			
+		</div>		
+	</div>  
+</div>
+
+<div id="derechos" class="container-fluid cont-gris contpadding">
+	<div class="row">		
+		<div class="col-xs-12 col-sm-6 text-center">
+			<h1 class="titulo ">CONOZCA SUS DERECHOS</h1>
+			<h2 class="">Leyes y Normativas</h2>			
+		</div>
+		<div class="col-xs-12 col-sm-6">
+			<p class="pull-right ">- LO PONDREMOS BIEN CONTRA LA DERECHA o desde el principio del div</p>
+			<p class="pull-right ">- LO PONDREMOS BIEN CONTRA LA DERECHA o desde el principio del div</p>
+			<p class="pull-right ">- LO PONDREMOS BIEN CONTRA LA DERECHA o desde el principio del div</p>
+			<p class="pull-right ">- LO PONDREMOS BIEN CONTRA LA DERECHA o desde el principio del div</p>
+			<p>- LO PONDREMOS BIEN CONTRA LA DERECHA o desde el principio del div</p>					
+		</div>		
+	</div>  
+</div>
+
+
+
+
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+   // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
+
+
+</script>
+
+</body>
+</html>
