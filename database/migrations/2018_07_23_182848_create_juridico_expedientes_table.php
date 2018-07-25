c7cf57bc9afb4fef6c5b5aa9e492dca4f70d2906
Martín Ghiglia <primo39@gmail.com>
2018-07-25 17:21:09 -0300

    avances en migrations

--- database/migrations/2018_07_23_182848_create_juridico_expedientes_table.php
+++ database/migrations/2018_07_23_182848_create_juridico_expedientes_table.php
@@ -15,6 +15,16 @@ class CreateJuridicoExpedientesTable extends Migration
     {
         Schema::create('juridico_expedientes', function (Blueprint $table) {
             $table->increments('id');
+			$table->integer('cliente_id');
+			$table->text('iue');
+			$table->text('caratula');
+			$table->text('juzgado');
+			$table->date('fecha_inicio');
+			$table->integer('user_id');
+			$table->integer('tipo_id');
+			$table->integer('estado_id');
+			$table->integer('paso_actual');
+
             $table->timestamps();
         });
     }
