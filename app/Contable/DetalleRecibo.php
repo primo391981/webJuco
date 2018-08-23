a45e1ce783011d980477946b2c48de10f1ea6af3
Fabian <fsellanes@gmail.com>
2018-08-23 20:05:30 -0300

    Recibos _ modelo

--- app/Contable/DetalleRecibo.php
+++ app/Contable/DetalleRecibo.php
@@ -3,8 +3,14 @@
 namespace App\Contable;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DetalleRecibo extends Model
 {
+	use SoftDeletes;
     protected $table = 'contable_detalles_recibo';
+	
+	public function conceptosRecibos() {
+        return $this->belongsTo('App\Contable\ConceptoRecibo','idConceptoRecibo');
+    }
 }
--- app/Contable/ReciboEmpleado.php
+++ app/Contable/ReciboEmpleado.php
@@ -3,8 +3,20 @@
 namespace App\Contable;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReciboEmpleado extends Model
 {
+	use SoftDeletes;
     protected $table = 'contable_recibos_empleado';
+	
+	protected $dates = ['deleted_at'];
+	
+	public function tiposRecibos() {
+        return $this->belongsTo('App\Contable\TipoRecibo','idTipoRecibo');
+    }
+	
+	public function detallesRecibos(){
+		return $this->hasMany('App\Contable\DetalleRecibo','idRecibo');
+	}
 }
