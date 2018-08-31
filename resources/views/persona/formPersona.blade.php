daa78d119862ebac05a04a822c3eb59a7c0d1562
Unknown <primo39@gmail.com>
2018-08-31 01:01:49 -0300

    mas correcciones

--- resources/views/persona/formPersona.blade.php
+++ resources/views/persona/formPersona.blade.php
@@ -83,4 +83,11 @@
 								@endif
 							</div>	
 						</div>
+						
+						<div class="form-group row">
+								<br>
+								<div class="col-xs-12 text-center">
+									<button type="submit" class="btn btn-warning btn-lg"><i class="fas fa-check"></i> Confirmar</button>
+								</div>
+						</div>
 					
